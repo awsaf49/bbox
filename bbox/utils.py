169765f42678ba70c427554b79586497602d0870
Awsaf <awsaf49@gmail.com>
2021-12-29 16:28:47 +0600

    plot and load_img added

--- bbox/utils.py
+++ bbox/utils.py
@@ -1,4 +1,7 @@
 from numba import jit
+import cv2
+import numpy as np
+import random
 
 @jit(nopython=True)
 def voc2yolo(bboxes, height=720, width=1280):
@@ -100,3 +103,96 @@ def coco2voc(bboxes, height=720, width=1280):
     bboxes[..., 2:4] += bboxes[..., 0:2]
     
     return bboxes
+
+def load_image(image_path):
+    return cv2.imread(image_path)[..., ::-1]
+
+
+def plot_one_box(x, img, color=None, label=None, line_thickness=None):
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+def draw_bboxes(img, bboxes, classes, class_ids, colors = None, show_classes = None, bbox_format = 'yolo', class_name = False, line_thickness = 2):  
+     
+    image = img.copy()
+    show_classes = classes if show_classes is None else show_classes
+    colors = (0, 255 ,0) if colors is None else colors
+    
+    if bbox_format == 'yolo':
+        
+        for idx in range(len(bboxes)):  
+            
+            bbox  = bboxes[idx]
+            cls   = classes[idx]
+            cls_id = class_ids[idx]
+            color = colors[cls_id] if type(colors) is list else colors
+            
+            if cls in show_classes:
+            
+                x1 = round(float(bbox[0])*image.shape[1])
+                y1 = round(float(bbox[1])*image.shape[0])
+                w  = round(float(bbox[2])*image.shape[1]/2) #w/2 
+                h  = round(float(bbox[3])*image.shape[0]/2)
+
+                voc_bbox = (x1-w, y1-h, x1+w, y1+h)
+                plot_one_box(voc_bbox, 
+                             image,
+                             color = color,
+                             label = cls if class_name else str(get_label(cls)),
+                             line_thickness = line_thickness)
+            
+    elif bbox_format == 'coco':
+        
+        for idx in range(len(bboxes)):  
+            
+            bbox  = bboxes[idx]
+            cls   = classes[idx]
+            cls_id = class_ids[idx]
+            color = colors[cls_id] if type(colors) is list else colors
+            
+            if cls in show_classes:            
+                x1 = int(round(bbox[0]))
+                y1 = int(round(bbox[1]))
+                w  = int(round(bbox[2]))
+                h  = int(round(bbox[3]))
+
+                voc_bbox = (x1, y1, x1+w, y1+h)
+                plot_one_box(voc_bbox, 
+                             image,
+                             color = color,
+                             label = cls if class_name else str(cls_id),
+                             line_thickness = line_thickness)
+
+    elif bbox_format == 'voc':
+        
+        for idx in range(len(bboxes)):  
+            
+            bbox  = bboxes[idx]
+            cls   = classes[idx]
+            cls_id = class_ids[idx]
+            color = colors[cls_id] if type(colors) is list else colors
+            
+            if cls in show_classes: 
+                x1 = int(round(bbox[0]))
+                y1 = int(round(bbox[1]))
+                x2 = int(round(bbox[2]))
+                y2 = int(round(bbox[3]))
+                voc_bbox = (x1, y1, x2, y2)
+                plot_one_box(voc_bbox, 
+                             image,
+                             color = color,
+                             label = cls if class_name else str(cls_id),
+                             line_thickness = line_thickness)
+    else:
+        raise ValueError('wrong bbox format')
+
+    return image
