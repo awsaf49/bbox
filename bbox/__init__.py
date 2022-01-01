1ae121a8c16ca921d39434e116ebfa334409761d
Awsaf <awsaf49@gmail.com>
2022-01-01 18:42:02 +0600

    refactor `__init__.py`

--- bbox/__init__.py
+++ bbox/__init__.py
@@ -1,3 +1,3 @@
-from .coco import Converter
-from .utils import voc2coco, coco2voc, yolo2coco, coco2yolo, voc2yolo, yolo2voc, bbox_iou, draw_bboxes, load_image
 from .version import __version__
+from bbox import coco
+from bbox import utils
